2ba688172489eaede9fe25f06f89a9daa1dc6307
Sebastian G. Q <sgq995@gmail.com>
2021-02-03 16:33:00 -0500

    feat: Added functional editor for defined posts

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,29 +1,61 @@
-import { useState } from 'react';
+import { Component } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
 
 import './Section.css';
 
-function Section({ title, icon }) {
-  const [items, setItems] = useState([]);
+class Section extends Component {
+  state = {
+    lastId: 0,
+    items: [],
+  }
 
-  return (
-    <section className="Section">
-      <header className="Section__header">
-        <h2 className="Section__title">{title}</h2>
-        <img className="Section__icon" alt={title} src={icon} />
-      </header>
+  handleNewItem = (content) => {
+    this.setState(({ lastId, items }) => {
+      return {
+        lastId: lastId + 1,
+        items: [...items, {
+          id: lastId,
+          content
+        }],
+      };
+    });
+  }
 
-      <article className="Section__content">
-        {items.map(content =>
-          <Post content={content} />
-        )}
+  handleEditItem = (id, content) => {
+    this.setState(({ lastId, items }) => {
+      const item = items.find(item => item.id === id);
+      item.content = content;
 
-        <NewPost onSubmit={(item) => setItems(items => [...items, item])} />
-      </article>
-    </section>
-  );
+      return {
+        lastId,
+        items,
+      };
+    });
+  }
+
+  render() {
+    const { title, icon } = this.props;
+    const { items } = this.state;
+
+    return (
+      <section className="Section">
+        <header className="Section__header">
+          <h2 className="Section__title">{title}</h2>
+          <img className="Section__icon" alt={title} src={icon} />
+        </header>
+
+        <article className="Section__content">
+          {items.map(({ id, content }) =>
+            <Post key={id} content={content} onEdit={content => this.handleEditItem(id, content)} />
+          )}
+
+          <NewPost onSubmit={this.handleNewItem} />
+        </article>
+      </section>
+    );
+  }
 }
 
 export default Section;
