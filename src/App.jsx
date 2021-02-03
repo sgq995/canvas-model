0c699a893166b00dd496f8d9fe35f5585dfdf1af
Sebastian G. Q <sgq995@gmail.com>
2021-02-03 14:42:31 -0500

    feat: Added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
 import Section from './components/Section';
 import Post from './components/Post';
-
-import './App.css';
 import NewPost from './components/NewPost';
 
-function App() {
-  const clients = [
-    'Estudiantes',
-    'Empresas',
-    'Emprendedores',
-  ];
+import './App.css';
 
+import keyPartnersIcon from './assets/key-partners.svg';
+import keyActivitiesIcon from './assets/key-activities.svg';
+import costStructureIcon from './assets/cost-structure.svg';
+import keyResourcesIcon from './assets/key-resources.svg';
+import valueIcon from './assets/value.svg';
+import clientsIcon from './assets/clients.svg';
+import channelsIcon from './assets/channels.svg';
+
+function App() {
   return (
     <div className="App">
       <div className="App__upper App__row">
-        <Section title="Aliados Clave" />
+        <Section title="Aliados Clave" icon={keyPartnersIcon} />
 
         <div className="App__column">
-          <Section title="Actividades Clave" />
-          <Section title="Recursos Clave" />
+          <Section title="Actividades Clave" icon={keyActivitiesIcon} />
+          <Section title="Recursos Clave" icon={keyResourcesIcon} />
         </div>
 
-        <Section title="Propuesta de Valor" />
+        <Section title="Propuesta de Valor" icon={valueIcon} />
 
         <div className="App__column">
           <Section title="Relación con Clientes" />
-          <Section title="Canales" />
+          <Section title="Canales" icon={channelsIcon} />
         </div>
 
-        <Section title="Segmento de Clientes">
-          {clients.map(client => <Post>{client}</Post>)}
-
-          <NewPost />
-        </Section>
+        <Section title="Segmento de Clientes" icon={clientsIcon} />
       </div>
 
       <div className="App__bottom App__row">
-        <Section title="Estructura de Costes" />
+        <Section title="Estructura de Costes" icon={costStructureIcon} />
         <Section title="Flujos de Ingreso" />
       </div>
     </div>
