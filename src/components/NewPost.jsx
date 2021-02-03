d8658d4579e0672f0e04e31a6dee6dc7a5a9a358
Sebastian G. Q <sgq995@gmail.com>
2021-02-03 15:35:22 -0500

    feat: New post style added

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,10 +1,30 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 
-import Post from "./Post";
+import "./NewPost.css";
 
 class NewPost extends Component {
   state = {
+    editable: false,
+    hasError: false,
+
     item: ''
+  }
+
+  inputRef = createRef(null);
+
+  handleEdit = () => {
+    this.setState({
+      editable: true
+    });
+
+    this.inputRef.current?.focus();
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.target.blur();
+      this.handleSubmit(event);
+    }
   }
 
   handleChange = (event) => {
@@ -19,23 +39,45 @@ class NewPost extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
+    if (this.state.item.trim().length === 0) {
+      this.setState({
+        hasError: true,
+      });
+      return;
+    }
+
+    this.setState({
+      editable: false,
+      hasError: false,
+    });
+
     const onSubmit = this.props.onSubmit ?? (() => { });
     onSubmit(this.state.item);
   }
 
   render() {
+    const { editable, hasError } = this.state;
+
     return (
-      <Post>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            name="item"
-            onChange={this.handleChange}
-          />
-          <button onClick={this.handleSubmit}>
-            Añadir
-          </button>
-        </form>
-      </Post>
+      <article className="NewPost">
+        {editable
+          ? <form className="NewPost__form" onSubmit={this.handleSubmit}>
+            <div className={`NewPost__field ${hasError && 'NewPost__field--error'}`} onClick={() => this.inputRef.current?.focus()}>
+              <input
+                ref={this.inputRef}
+                className="NewPost__input"
+                name="item"
+                onKeyPress={this.handleKeyPress}
+                onChange={this.handleChange}
+              // onBlur={this.handleSubmit}
+              />
+              <button className="NewPost__submit material-icons">done</button>
+            </div>
+          </form>
+          : <button className="NewPost__button material-icons" onClick={this.handleEdit}>add</button>
+        }
+
+      </article>
     );
   }
 }
